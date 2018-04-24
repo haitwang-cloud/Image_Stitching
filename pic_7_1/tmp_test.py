41d85c451551f71090e2ba4c05035a169299c98f
wanghaitao <wanghaitao@i.shu.edu.cn>
2018-04-24 10:16:53 +0800

    Signed-off-by: wanghaitao <wanghaitao@i.shu.edu.cn>

---
+++ pic_7_1/tmp_test.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import math
+tmp = []
+for i in range(256):
+    tmp.append(0)
+val = 0
+k = 0
+res = 0
+image = cv2.imread('1.tif',0)
+img = np.array(image)
+for i in range(len(img)):
+    for j in range(len(img[i])):
+        val = img[i][j]
+        tmp[val] = float(tmp[val] + 1)
+        k =  float(k + 1)
+for i in range(len(tmp)):
+    tmp[i] = float(tmp[i] / k)
+for i in range(len(tmp)):
+    if(tmp[i] == 0):
+        res = res
+    else:
+        res = float(res - tmp[i] * (math.log(tmp[i]) / math.log(2.0)))
+print res
